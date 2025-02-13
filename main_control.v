ceef6c365f31e03e53178f8c479740b7bf148fa0
Kushal Gowda C R <kushal@Kushals-MacBook-Air.local>
2025-02-13 14:57:09 +0530

    update0.1

--- main_control.v
+++ main_control.v
@@ -23,15 +23,15 @@ module main_control #(
     reg [10:0] compute_cycles;
     reg [1:0] act_fn_index;
 
-    localparam IDLE       = 5'b00000,
-               MEM_DELAY  = 5'b00001,
-               LOAD_FIFO  = 5'b00010,
-               COMPUTE    = 5'b00100,
-               ACTIVATE   = 5'b01000,
-               FEED_NEXT  = 5'b10000;
+    localparam IDLE       = 4'b0000,
+              // MEM_DELAY  = 5'b00001,
+               LOAD_FIFO  = 4'b0001,
+               COMPUTE    = 4'b0010,
+               ACTIVATE   = 4'b0100,
+               FEED_NEXT  = 5'b1000;
 
     always @(posedge clk ) begin
-        
+
         if (rst) begin
             state <= IDLE;
             counter <= 0;
@@ -46,27 +46,18 @@ module main_control #(
             layer_no <= 0;
             compute_cycles <= NO_INPUTS_FL + 1;
             act_fn_index <= 0;
-
         end else begin
 
             case (state)
                 IDLE: begin
                     counter <= 0;
                     arb_en <= 1'b1;
-                    if (arb_en) state <= MEM_DELAY;
+                    if (arb_en) state <= LOAD_FIFO;
                 end
-                
-                MEM_DELAY: begin
-                    counter <= counter + 1;
-                    if (counter == 2) begin
-                        counter <= 0;
-                        state <= LOAD_FIFO;
-                    end
-                end
-
+            
                 LOAD_FIFO: begin
                     counter <= counter + 1;
-                    if (counter == 3) begin
+                    if (counter == 2) begin
                         counter <= 0;
                         state <= COMPUTE;
                     end
@@ -74,7 +65,7 @@ module main_control #(
                 
                 COMPUTE: begin
                     counter <= counter + 1;
-                    if (counter >= 4) begin // FIFO delay of 4 cycles
+                    if (counter >= 4) begin 
                         rd_en <= 4'b1111;
                         mac_en <= 4'b1111;
                     end
@@ -87,22 +78,23 @@ module main_control #(
                 end
                 
                 ACTIVATE: begin
-                    act_fn_en <= 4'b0000;
-                    act_fn_en[act_fn_index] <= 1'b1;
-                    act_fn_index <= act_fn_index + 1;
-                    if (act_fn_index == 3) begin
-                        act_fn_index <= 0;
-                        state <= FEED_NEXT;
-                    end
+                    act_fn_en <= 4'b1111;
+                    state <= FEED_NEXT;
+                    // act_fn_en[act_fn_index] <= 1'b1;
+                    // act_fn_index <= act_fn_index + 1;
+                    // if (act_fn_index == 3) begin
+                    //     act_fn_index <= 0;
+                    //     state <= FEED_NEXT;
+                    // end
                 end
                 
                 FEED_NEXT: begin
                     feed_through <= 1'b1;
                     arb_en <= 1'b1;
                     layer_no <= layer_no + 1;
-                    if (layer_no == 0) compute_cycles <= NO_NEURONS_HL + 1;
+                    if (layer_no == 0) compute_cycles <= NO_NEURONS_FL + 1;
                     else if (layer_no == NO_HIDDEN_LAYERS) compute_cycles <= NO_NEURONS_OL + 1;
-                    else compute_cycles <= NO_NEURONS_HL + 1;
+                    else compute_cycles <= NO_NEURONS_HL;
                     state <= COMPUTE;
                 end
             endcase
